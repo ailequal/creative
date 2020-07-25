c3da96c06cb5a8e38315af70089384917b223afa
ailequal <37016865+ailequal@users.noreply.github.com>
2020-07-25 18:08:14 +0200

    Minor fix

--- creative/functions.php
+++ creative/functions.php
@@ -7,11 +7,6 @@
  * @package creative
  */
 
-if ( ! defined( '_S_VERSION' ) ) {
-	// Replace the version number of the theme on each release.
-	define( '_S_VERSION', '1.0.0' );
-}
-
 /**
  * Scripts per il funzionamento del tema.
  */
